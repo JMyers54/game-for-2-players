d9b6c11852ce23354f60707651d63dec81eda313
santiagoduque722 <santiagodque24@gmail.com>
2025-05-30 14:26:49 -0500

    Se actualizo el archivo controles

--- controles.py
+++ controles.py
@@ -1,32 +1,22 @@
+def configurar_controles(ventana, jugador1, jugador2):
+    def jugador1_arriba():
+        y = jugador1.ycor()
+        jugador1.sety(y + 25)
 
+    def jugador1_abajo():
+        y = jugador1.ycor()
+        jugador1.sety(y - 25)
 
+    def jugador2_arriba():
+        y = jugador2.ycor()
+        jugador2.sety(y + 25)
 
-class Configuraciones():
-    def __init__(self):
-    
-    def configurar_controles(ventana, jugador1, jugador2):
-        def jugador1_arriba():
-            y = jugador1.ycor()
-            y += 25
-            jugador1.sety(y)
+    def jugador2_abajo():
+        y = jugador2.ycor()
+        jugador2.sety(y - 25)
 
-        def jugador1_abajo():
-            y = jugador1.ycor()
-            y -= 25
-            jugador1.sety(y)
-
-        def jugador2_arriba():
-            y = jugador2.ycor()
-            y += 25
-            jugador2.sety(y)
-
-        def jugador2_abajo():
-            y = jugador2.ycor()
-            y -= 25
-            jugador2.sety(y)
-
-        ventana.listen()
-        ventana.onkeypress(jugador1_arriba, "w")
-        ventana.onkeypress(jugador1_abajo, "s")
-        ventana.onkeypress(jugador2_arriba, "Up")
-        ventana.onkeypress(jugador2_abajo, "Down")
+    ventana.listen()
+    ventana.onkeypress(jugador1_arriba, "w")
+    ventana.onkeypress(jugador1_abajo, "s")
+    ventana.onkeypress(jugador2_arriba, "Up")
+    ventana.onkeypress(jugador2_abajo, "Down")
